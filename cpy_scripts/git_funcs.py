a8a78456bb16f98c7f804283e67ee31ab31db083
Alec Delaney <tekktrik@gmail.com>
2022-05-29 23:25:28 -0400

    Add git functionality (2nd)

--- cpy_scripts/git_funcs.py
+++ cpy_scripts/git_funcs.py
@@ -5,7 +5,10 @@ import git.repo
 import git.index.base
 from cpy_scripts.lib_funcs import StrPath, LibFunc
 
-def _get_repo_and_remote(lib_path: StrPath, remote_name: str = "origin") -> Tuple[git.repo.Repo, git.Remote]:
+
+def _get_repo_and_remote(
+    lib_path: StrPath, remote_name: str = "origin"
+) -> Tuple[git.repo.Repo, git.Remote]:
 
     # Create the repo and remote objects
     repo = git.repo.Repo(lib_path)
@@ -13,11 +16,14 @@ def _get_repo_and_remote(lib_path: StrPath, remote_name: str = "origin") -> Tupl
 
     return repo, remote
 
-def sync_repo(lib_path: StrPath, remote_name: str = "origin", branch_name: str = "main") -> None:
+
+def sync_repo(
+    lib_path: StrPath, remote_name: str = "origin", branch_name: str = "main"
+) -> None:
     """
     Update the repo, and ensure it is on the given branch using a
     forced checkout
-    
+
     :param StrPath lib_path: The path to the repo
     :param str remote_name: The remote name to fetch and pull,
         default is ``origin``
@@ -27,20 +33,21 @@ def sync_repo(lib_path: StrPath, remote_name: str = "origin", branch_name: str =
 
     # Create the repo and remote objects
     repo, remote = _get_repo_and_remote(lib_path, remote_name)
-    
+
     # Fetch from the remote
     remote.fetch()
 
     # Checkout and pull to the given branchb
-    #if repo.active_branch != branch_name:
+    # if repo.active_branch != branch_name:
     branch: git.Head = getattr(repo.heads, branch_name)
     branch.checkout(force=True)
     remote.pull()
 
+
 def push_changes(lib_path: StrPath, remote_name: str = "origin") -> None:
     """
     Pushes any changes made to the repo to the given remote
-    
+
     :param StrPath lib_path: The path to the repo
     :param str remote_name: (optional) The name of remote, default
         is ``main``
@@ -52,7 +59,13 @@ def push_changes(lib_path: StrPath, remote_name: str = "origin") -> None:
     # Push changes
     remote.push()
 
-def commit_changes(lib_path: StrPath, message:str, remote_name: str = "origin", skip_hooks: bool = True) -> None:
+
+def commit_changes(
+    lib_path: StrPath,
+    message: str,
+    remote_name: str = "origin",
+    skip_hooks: bool = True,
+) -> None:
 
     # Create the repo and remote objects
     repo, _ = _get_repo_and_remote(lib_path, remote_name)
@@ -62,9 +75,15 @@ def commit_changes(lib_path: StrPath, message:str, remote_name: str = "origin", 
     index_file.commit(message, skip_hooks=skip_hooks)
 
 
-def sync_commit_push(message: str, *, remote_name: str = "origin", branch_name: str = "main", skip_hooks: bool = True):
+def sync_commit_push(
+    message: str,
+    *,
+    remote_name: str = "origin",
+    branch_name: str = "main",
+    skip_hooks: bool = True
+):
     """
-    Decorator for automatically fetching, pulling, and pushing changes 
+    Decorator for automatically fetching, pulling, and pushing changes
     for a library function
 
     :param str remote_name: (optional) The name of the remote, default
@@ -72,10 +91,11 @@ def sync_commit_push(message: str, *, remote_name: str = "origin", branch_name: 
     :param str branch_name: (optional) The name of the branch, default
         is ``main``
     """
-    
+
     def decorator_sync_commit_push(func):
 
         functools.wraps(func)
+
         def wrapper_sync_commit_push(lib_path: StrPath, *args, **kwargs) -> Any:
 
             # Fetch and pull to repo
